b0e9c1f8573c75cbd5241c51181cd6c4a72015a1
Jick <Jick@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-29 23:46:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/habla-for-wordpress/trunk@150435 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ habla-for-wordpress.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Hab.la for WordPress
+Plugin URI: http://www.jamesdimick.com/creations/habla-for-wordpress/
+Description: A plugin that allows website authors to easily place a <a href="http://www.hab.la/">Hab.la</a> live help widget on their WordPress website.
+Author: James Dimick
+Author URI: http://www.jamesdimick.com/
+
+=== LEGAL INFORMATION ===
+  Copyright (C) 2009 James Dimick <mail@jamesdimick.com> - www.jamesdimick.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
